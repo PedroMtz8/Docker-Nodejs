43ea32a42fc1f2c246591c4a602d120d99dbde54
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-11-28 21:12:40 -0600

    creating docker container and configurated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import express from 'express';
-
+import { createPool } from 'mysql2/promise'
+import { config } from 'dotenv';
+config();
+console.log(process.env.MYSQLDB_DATABASE)
 const app = express();
+const pool = createPool({
+  host: process.env.MYSQLDB_HOST,
+  user: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB__PASSWORD,
+  port: process.env.MYSQLDB_DOCKER_PORT
+})
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-const PORT = 3000;
+app.get('/ping', async (req, res) => {
+  const result = await pool.query('SELECT NOW()')
+  res.send(result[0]);
+});
+
+const PORT = process.env.NODE_LOCAL_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`http://localhost:${PORT}`);
+  }
 });
